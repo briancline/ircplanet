9f107a04af6dcfdcf717b875deeb1dd07803da5a
Brian Cline <brian.cline@gmail.com>
2008-11-20 01:59:51 +0000

    Added missing topic command handler in operator service.

---
+++ Operator/commands/topic.php
@@ -0,0 +1,21 @@
+<?php
+
+	if( !($chan = $this->get_channel($chan_name)) ) {
+		$bot->noticef( $user, "Nobody is on channel %s.", $chan_name );
+		return false;
+	}
+	
+	if( $cmd_num_args < 2 )
+	{
+		$bot->noticef( $user, 'Topic on %s%s%s is currently: %s',
+			BOLD_START, $chan->get_name(), BOLD_END,
+			$chan->get_topic() );
+	}
+	else
+	{
+		$new_topic = assemble( $pargs, 2 );
+		$bot->topic( $chan->get_name(), $new_topic, $chan->get_ts() );
+		$chan->topic = $new_topic;
+	}
+	
+?>
