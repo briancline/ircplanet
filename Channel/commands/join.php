24f3d68e2353988412abb901686a0397a022bac7
Brian Cline <brian.cline@gmail.com>
2008-12-15 17:48:44 +0000

    Forgot to add command files a few revisions ago for CS JOIN/PART.

---
+++ Channel/commands/join.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * ircPlanet Services for ircu
+ * Copyright (c) 2005 Brian Cline.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of ircPlanet nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without 
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+	$chan = $this->get_channel( $chan_name );
+
+	if( $chan && $chan->is_on($bot->get_numeric()) )
+	{
+		$bot->noticef( $user, "I am already in %s.", $chan_name );
+		return false;
+	}
+
+	if( $chan )
+	{
+		/**
+		 * At least one other person is already in the channel, so
+		 * just issue a JOIN and op the bot.
+		 */
+		$bot->join( $chan->get_name() );
+		$this->op( $chan->get_name(), $bot->get_numeric() );
+	}
+	else
+	{
+		/**
+		 * Nobody is in this channel, so it doesn't exist. Bot::join
+		 * will issue a CREATE in this case.
+		 */
+		$chan_reg = $this->get_channel_reg( $chan_name );
+		$bot->join( $chan_name, $chan_reg->get_create_ts() );
+	}
+	
+?>
