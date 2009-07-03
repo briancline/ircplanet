4f5529b6c84f11aa311d73a32617f4cf6e0551d2
Brian Cline <brian.cline@gmail.com>
2009-07-03 21:45:05 +0000

    Fix crash bug in OS - occurs when receiving a g-line deactiviation for a g-line OS doesn't know about.

--- Operator/p10/handle_GL.php
+++ Operator/p10/handle_GL.php
@@ -32,11 +32,14 @@
 	$server = $this->get_server( $args[0] );
 	$gline = $this->get_gline( $mask );
 	
-	if($add)
-		$mask = "+$mask";
-	else
-		$mask = "-$mask";
-	
-	$this->report_event( 'GLINE', $server, $gline, '('. $gline->get_reason() .')' );
+	if( $gline )
+	{
+		if( $add )
+			$mask = "+$mask";
+		else
+			$mask = "-$mask";
+		
+		$this->report_event( 'GLINE', $server, $gline, '('. $gline->get_reason() .')' );
+	}
 
 ?>
